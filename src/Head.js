13fb98e4ac52982b2d45a46e082872ffb8a6c961
Tuhh16 <arthur.alves.lima@live.com>
2021-03-20 15:18:25 -0300

    Ajuste no header

---
+++ src/Head.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import MetaTags from 'react-meta-tags';
+//import Imagem from '../imagem-app.jpg';
+
+function Head(){
+    return(
+        <MetaTags>
+            <title>Contato</title>
+            <meta property="og:title" content="Contato" content="Contato" key="Contato" />
+            <meta name="description" content="Formulários de Contato." />
+            <meta property="og:image" content={`${process.env.PUBLIC_URL}/img/image-app.jpg`}/>
+            <meta property="og:type" content="website" />
+            <meta property="og:url" content="https://react-contact-form-front-end.vercel.app" />
+        </MetaTags>
+    )
+}
+
+export default Head
--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Head from './Head';
 import App from './App';
 
 ReactDOM.render(
   <React.StrictMode>
+    <Head/>
     <App />
   </React.StrictMode>,
   document.getElementById('root')
